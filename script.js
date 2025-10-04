cac601665e232d65a884de4569fe72878497046f
Luke <lucafinaldi3@gmail.com>
2025-10-04 20:34:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-// Code Translator)
-  const cardOne = document.getElementById("tool-1");
-  if (cardOne) {
-    cardOne.style.cursor = "pointer";
-    cardOne.addEventListener("click", () => {
-      window.location.href = "https://github.com/lxfx05/Code-translator";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const firstTool = document.getElementById("tool-1");
+
+  if (firstTool) {
+    firstTool.style.cursor = "pointer";
+    firstTool.addEventListener("click", () => {
+      window.location.href = "https://lxfx05.github.io/Code-translator/";
 });
 }
 });
