293adda52bc67b2025ae88f871e22dab0aabb5ce
Luke <lucafinaldi3@gmail.com>
2025-12-07 09:33:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   const firstTool = document.getElementById("tool-1");
   const secondTool = document.getElementById("tool-2");
-  const thirdTool = document.getElementById("tool-3"); // Free Bot
+  const thirdTool = document.getElementById("tool-3");
 
-  // Primo tool
   if (firstTool) {
     firstTool.style.cursor = "pointer";
     firstTool.addEventListener("click", () => {
       window.location.href = "https://code-translator-xi-dusky.vercel.app/";
-    });
-  }
-// Secondo tool
-if (secondTool) {
-  secondTool.style.cursor = "default";
-  secondTool.addEventListener("click", () => {
-    secondTool.textContent = "Project in via of deployedment, but now is not aviable, come soon later";
-    secondTool.style.color = "#ffdddd";
 });
 }
 
-// Terzo tool
-if (thirdTool) {
-  thirdTool.style.cursor = "default";
-  thirdTool.addEventListener("click", () => {
-    thirdTool.textContent = "Project in via of deployement, but now is aviable";
-    thirdTool.style.color = "#ffdddd";
-    window.location.href = "https://github.com/lxfx05/Code-gpt";
+  if (secondTool) {
+    secondTool.style.cursor = "default";
+    secondTool.addEventListener("click", () => {
+      secondTool.textContent = "Project in via of deployedment, but now is not aviable, come soon later";
+      secondTool.style.color = "#ffdddd";
 });
 }
+
+  if (thirdTool) {
+    thirdTool.style.cursor = "default";
+    thirdTool.addEventListener("click", () => {
+      thirdTool.textContent = "Project in via of deployedment, but now is not aviable, come soon later";
+      thirdTool.style.color = "#ffdddd";
+      window.location.href = "https://github.com/lxfx05/Code-gpt";
+});
+}
+});
